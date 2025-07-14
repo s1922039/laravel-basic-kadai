02c2c63cd61fc88f62f0e8844244e14f77659856
s1922039 <danganms22@gmail.com>
2025-07-14 10:56:47 +0900

    クエリビルダ

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,10 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>課題</title>
+    <title>クエリビルダ</title>
 </head>
 <body>
     <h1>投稿一覧</h1>
+    <table>
+        <tr>
+            <th>タイトル</th>
+            <th>本文</th>
+        </tr>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->content }}</td>
+            </tr>
+        @endforeach
+    </table>
 </body>
 
 </html>
